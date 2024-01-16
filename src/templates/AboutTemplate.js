514b66e2d7f57c30b111c28dc3c827ad81d94ae2
Tim Viklund Fjesme <tim.viklund.fjesme@iths.se>
2024-01-16 14:41:38 +0100

    added title to other pages

--- src/templates/AboutTemplate.js
+++ src/templates/AboutTemplate.js
@@ -1,18 +1,24 @@
 import React from "react"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { GatsbyImage } from "gatsby-plugin-image"
+import { Helmet } from "react-helmet"
 
 const AboutTemplate = ({ title, content, image }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      {content &&
-        content.raw &&
-        documentToReactComponents(JSON.parse(content.raw))}
-      {image && image.gatsbyImageData && (
-        <GatsbyImage image={image.gatsbyImageData} alt={title} />
-      )}
-    </div>
+    <>
+      <Helmet>
+        <title>About</title>
+      </Helmet>
+      <div>
+        <h1>{title}</h1>
+        {content &&
+          content.raw &&
+          documentToReactComponents(JSON.parse(content.raw))}
+        {image && image.gatsbyImageData && (
+          <GatsbyImage image={image.gatsbyImageData} alt={title} />
+        )}
+      </div>
+    </>
   )
 }
 
--- src/templates/ContactTemplate.js
+++ src/templates/ContactTemplate.js
@@ -2,27 +2,33 @@ import React from "react"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { GatsbyImage } from "gatsby-plugin-image"
 import ContactForm from "../components/ContactForm"
+import { Helmet } from "react-helmet"
 
 const ContactTemplate = ({ title, content, image }) => {
   return (
-    <div className="contact-template">
-      <div className="contact-header">
-        <h1>{title}</h1>
+    <>
+      <Helmet>
+        <title>Contact</title>
+      </Helmet>
+      <div className="contact-template">
+        <div className="contact-header">
+          <h1>{title}</h1>
+        </div>
+        <div className="contact-content">
+          {content &&
+            content.raw &&
+            documentToReactComponents(JSON.parse(content.raw))}
+        </div>
+        <div className="contact-form">
+          <ContactForm />
+        </div>
+        <div className="contact-image">
+          {image && image.gatsbyImageData && (
+            <GatsbyImage image={image.gatsbyImageData} alt={title} />
+          )}
+        </div>
       </div>
-      <div className="contact-content">
-        {content &&
-          content.raw &&
-          documentToReactComponents(JSON.parse(content.raw))}
-      </div>
-      <div className="contact-form">
-        <ContactForm />
-      </div>
-      <div className="contact-image">
-        {image && image.gatsbyImageData && (
-          <GatsbyImage image={image.gatsbyImageData} alt={title} />
-        )}
-      </div>
-    </div>
+    </>
   )
 }
 
